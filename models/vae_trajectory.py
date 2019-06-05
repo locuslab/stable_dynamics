319e8af09ea6d86d9ad88e2b4a7aa199b79da7ed
Gaurav Manek <gaurav@gauravmanek.com>
2019-06-05 15:26:24 -0400

    Prevent image saturation

--- models/vae_trajectory.py
+++ models/vae_trajectory.py
@@ -43,8 +43,8 @@ def summary(epoch, summarywriter, Ypred, X):
     Y_a, mu_a, logvar_a, z_a, Y_b, z_b = Ypred
     X_now, X_next = X
     summarywriter.add_embedding(z_a.data, label_img=X_now.data, global_step=epoch, tag="learned_embedding")
-    summarywriter.add_images("current_reconstructed", Y_a, global_step=epoch)
-    summarywriter.add_images("next_reconstructed", Y_b, global_step=epoch)
+    summarywriter.add_images("current_reconstructed", Y_a.clamp(max=1.0), global_step=epoch)
+    summarywriter.add_images("next_reconstructed", Y_b.clamp(max=1.0), global_step=epoch)
 
 def loss_flatten(l):
     return l
